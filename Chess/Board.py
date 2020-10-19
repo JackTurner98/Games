63589338097f30265feb85ed26ed40ba7956f82d
Jack Turner <jackturner@cmail.carleton.ca>
2020-10-19 01:16:36 -0400

    Stop using Tile, board is None or Pieces

--- Chess/Board.py
+++ Chess/Board.py
@@ -1,3 +1,4 @@
+from Pawn import Pawn
 from Tile import Tile
 # 1  [1] [0] [1] [0] [1] [0] [1] [0]
 # 2  [0] [1] [0] [1] [0] [1] [0] [1]
@@ -10,23 +11,64 @@ from Tile import Tile
 #     A   B   C   D   E   F   G   H
 
 class Board:
+    BLACK_KING_ROW = 0
+    BLACK_PAWN_ROW = 1
+    WHITE_KING_ROW = 7
+    WHITE_PAWN_ROW = 6
+
+    PIECES = {Pawn}
     def __init__(self):
         self.size = 8
+        self.pieces = []
         self.tiles = [[None for x in range(self.size)] for x in range(self.size)]
-        self.createBoard()
+        #self.createBoard()
+        self.fillPieces()
 
     # Fills the tiles array with alternating colors
     def createBoard(self):
         currColor = 0  # 1 == White; 0 == Black
-        for i in range(self.size):      # Numbers
-            for j in range(self.size):  # Letters
-                self.tiles[i][j] = Tile(currColor)
+        for i in self.tiles:      # Numbers
+            for j, tile in enumerate(i):  # Letters
+                i[j] = Tile(currColor)
                 currColor = int(not currColor)
             currColor = int(not currColor)
 
+    def fillPieces(self):
+        # Pawn Row
+        for i in range(self.size):
+            self.tiles[self.BLACK_PAWN_ROW][i] = Pawn(0)
+            self.tiles[self.WHITE_PAWN_ROW][i] = Pawn(1)
+
+        # # Rooks
+        # self.tiles[self.BLACK_KING_ROW][0] = Rook(0)
+        # self.tiles[self.BLACK_KING_ROW][7] = Rook(0)
+        #
+        # self.tiles[self.WHITE_KING_ROW][0] = Rook(1)
+        # self.tiles[self.WHITE_KING_ROW][7] = Rook(1)
+        #
+        # # Knights
+        # self.tiles[self.BLACK_KING_ROW][1] = Knight(0)
+        # self.tiles[self.BLACK_KING_ROW][6] = Knight(0)
+        #
+        # self.tiles[self.WHITE_KING_ROW][1] = Knight(1)
+        # self.tiles[self.WHITE_KING_ROW][6] = Knight(1)
+        # # Bishops
+        # self.tiles[self.BLACK_KING_ROW][2] = Bishop(0)
+        # self.tiles[self.BLACK_KING_ROW][5] = Bishop(0)
+        #
+        # self.tiles[self.WHITE_KING_ROW][2] = Bishop(1)
+        # self.tiles[self.WHITE_KING_ROW][5] = Bishop(1)
+        #
+        # # Queen
+        # self.tiles[self.BLACK_KING_ROW][3] = Queen(0)
+        # self.tiles[self.WHITE_KING_ROW][3] = Queen(1)
+        #
+        # # King
+        # self.tiles[self.BLACK_KING_ROW][4] = King(0)
+        # self.tiles[self.WHITE_KING_ROW][4] = King(1)
+
     # Prints a pretty version of the game board
     def printBoard(self):
-
         lineStr = "  "
         for i in range(self.size):
             lineStr += " {} ".format(i)
@@ -42,4 +84,30 @@ class Board:
         for i in range(self.size):
             lineStr += " {} ".format(i)
         print(lineStr)
+
+    def printBoard(self):
+        lineStr = "  "
+        for i in range(self.size):
+            lineStr += " {} ".format(i)
+        print(lineStr)
+
+        for i, ytile in enumerate(self.tiles):
+            lineStr = "{} ".format(i)
+            for j in ytile:
+                if j is not None:
+                    lineStr += "[{}]".format(j.getColor())
+                else:
+                    lineStr += "[ ]"
+            print(lineStr + " {}".format(i))
+
+        lineStr = "  "
+        for i in range(self.size):
+            lineStr += " {} ".format(i)
+        print(lineStr)
+
+    def getTiles(self):
+        return self.tiles
+
+    def getSize(self):
+        return self.size
 
